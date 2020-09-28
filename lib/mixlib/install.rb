b8130472ef850e7e4c1dcf5ff4ca163f8912ea36
Tensibai Zhaoying <tensibai@gmail.com>
2020-09-28 14:52:03 +0000

    Update lib/mixlib/install.rb

--- lib/mixlib/install.rb
+++ lib/mixlib/install.rb
@@ -119,7 +119,7 @@ module Mixlib
       # This only works for chef and chefdk but they are the only projects
       # we are supporting as of now.
       if options.for_ps1?
-        "$env:systemdrive\\opscode\\#{options.product_name}"
+        "$env:systemdrive\\#{Mixlib::Install::Dist::WINDOWS_INSTALL_DIR}\\#{options.product_name}"
       else
         "/opt/#{options.product_name}"
       end
@@ -136,7 +136,7 @@ module Mixlib
       # chef-server -> /opt/opscode). But this is OK for now since
       # chef & chefdk are the only supported products.
       version_manifest_file = if options.for_ps1?
-                                "$env:systemdrive\\opscode\\#{options.product_name}\\version-manifest.json"
+                                "$env:systemdrive\\#{Mixlib::Install::Dist::WINDOWS_INSTALL_DIR}\\#{options.product_name}\\version-manifest.json"
                               else
                                 "/opt/#{options.product_name}/version-manifest.json"
                               end
