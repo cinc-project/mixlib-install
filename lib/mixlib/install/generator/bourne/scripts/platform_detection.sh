593acbbf6e52b1b0645011f976cd7447fee3443f
Lance Albertson <lance@osuosl.org>
2025-09-11 11:12:26 -0700

    Have Rocky Linux use major like the other EL platforms
    This is causing issues with ppc64le for some reason
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- lib/mixlib/install/generator/bourne/scripts/platform_detection.sh
+++ lib/mixlib/install/generator/bourne/scripts/platform_detection.sh
@@ -157,6 +157,9 @@ case $platform in
     # FIXME:  "el" is deprecated, should use "redhat"
     platform_version=$major_version
     ;;
+  "rocky")
+    platform_version=$major_version
+    ;;
   "debian")
     if [ "$major_version" = "5" ]; then
       # This is here for potential back-compat.
