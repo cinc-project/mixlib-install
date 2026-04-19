0fdfe97cb73622f2a63b625e5bbe2d2724660c03
Lance Albertson <lance@osuosl.org>
2026-04-19 14:24:19 -0700

    Add GitLab CI publish gem job
    - Add .gitlab-ci.yml with publish-gems stage triggered from web UI
    - Add publish-gems.sh script to build and push gem to rubygems.cinc.sh
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

---
+++ publish-gems.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+#
+# Author:: Lance Albertson <lance@osuosl.org>
+# Copyright:: Copyright 2020-2026, Cinc Project
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export PATH="/opt/omnibus-toolchain/embedded/bin/:${PATH}"
+
+source /home/omnibus/load-omnibus-toolchain.sh
+set -x
+gem build mixlib-install.gemspec
+VERSION="$(cat VERSION)"
+gem push mixlib-install-${VERSION}.gem --host https://rubygems.cinc.sh
